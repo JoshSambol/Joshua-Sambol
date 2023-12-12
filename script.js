c05d0c43b401e729f27fdfb5f0772177741a5569
LanaSambol14 <153039645+LanaSambol14@users.noreply.github.com>
2023-12-12 17:42:33 -0500

    add confetti to script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+import confetti from 'https://cdn.skypack.dev/canvas-confetti';
+
+const button = document.getElementById('button');
+
+function makeConfetti(){
+  confetti();
+}
+
+button.addEventListener("click", makeConfetti)
 
