3c95ee655b0b7f6cfc7dfb1cafd78ad8c911315e
LanaSambol14 <153039645+LanaSambol14@users.noreply.github.com>
2023-12-19 23:13:45 +0000

    Update

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 import confetti from 'https://cdn.skypack.dev/canvas-confetti';
 
-const button = document.getElementById('confetti');
+const btn = document.querySelector('.ok');
+const container = document.querySelector('.container');
+const inv = document.querySelector('.inv');
 
+console.log(btn)
 function makeConfetti(){
   confetti();
   confetti();
   confetti();
 }
 
-button.addEventListener("click", makeConfetti)
+btn.addEventListener("click", ()=>{
+  confetti();
+  confetti();
+  confetti();
+  container.style.display="flex";
+  inv.style.display="none";
+  
+})
 
