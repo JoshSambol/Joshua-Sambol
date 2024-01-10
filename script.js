0a635f9b3c29462dc5f2b56776bc2dba60479d27
LanaSambol14 <153039645+LanaSambol14@users.noreply.github.com>
2024-01-10 03:35:06 +0000

    "fix minor stuff"

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ import confetti from 'https://cdn.skypack.dev/canvas-confetti';
 
 const btn = document.querySelector('.ok');
 const container = document.querySelector('.container');
+const enter = document.querySelector('.enter');
 const inv = document.querySelector('.inv');
+
+
+
 
 console.log(btn)
 function makeConfetti(){
@@ -20,3 +24,14 @@ btn.addEventListener("click", ()=>{
   
 })
 
+enter.addEventListener('click', ()=>{
+  console.log("here")
+  var a = document.querySelector('.ps');
+  var ps = a.value
+  if (ps == "Q!W@E#R$"){
+    window.alert("You're in!")
+  }
+  else{
+    window.alert("Incorrect passcode.")
+  }
+});
