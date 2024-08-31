f3ea0a54a04353d27d2fe7c18f95637c40586c05
Joshy14 <97353253+Joshy14@users.noreply.github.com>
2024-08-31 00:17:55 +0000

    Scrolling effects, Buy me a coffee, and more updates

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-document.getElementById('dark-mode-toggle').addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-});
+
+const homeButton = document.querySelector('.homeButton')
+const projectButton = document.querySelector('.projectButton')
+
+const home = document.querySelector('.home')
+const projects = document.querySelector('.projects')
+
+// JavaScript to add the 'visible' class when elements are in view
+const scroll = document.querySelectorAll('.scroll');
+
+function checkVisibility() {
+  const windowHeight = window.innerHeight;
+  scroll.forEach(element => {
+    const rect = element.getBoundingClientRect();
+    if (rect.top < windowHeight && rect.bottom >= 0) {
+      element.classList.add('visible');
+    } else {
+      element.classList.remove('visible');
+    }
+  });
+}
+
+window.addEventListener('scroll', checkVisibility);
+checkVisibility(); // Check visibility on page load
+
+homeButton.addEventListener('click', ()=>{
+  projects.style.display = 'none'
+  home.style.display = 'flex'
+})
+
+projectButton.addEventListener('click', ()=>{
+  home.style.display = 'none'
+  projects.style.display = 'flex'
+})
+
+document.getElementById('dark-mode-toggle').addEventListener('click', ()=>{
+  document.body.classList.toggle('dark-mode')
+})
+
